eea9bab069e88f430f13c3a980d2f9936434e97f
Mohamed El-Zeneiny <melzeneiny@uwaterloo.ca>
2020-07-10 13:42:48 -0400

    Done with persistent redos

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,46 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.undoList = []
+        self.redoList = []
 
     def add(self, num: int):
-        pass
+        self.value += num
+        self.undoList += [("+",num)]
+        print(str(self.undoList) + "  ,  " + str(self.value))
+        #self.redoList = []
+
 
     def subtract(self, num: int):
-        pass
+        self.value -= num
+        self.undoList += [("-",num)]
+        #self.redoList = []
 
     def undo(self):
-        pass
+        if (len(self.undoList) == 0):
+            return
+        if (self.undoList[-1][0] == "+"):
+            self.value -= self.undoList[-1][1]
+        else:
+            self.value += self.undoList[-1][1]
+        self.redoList += self.undoList[-1:]
+        del self.undoList[-1]
+
 
     def redo(self):
-        pass
+        if (len(self.redoList) == 0):
+            return
+        if (self.redoList[-1][0] == "+"):
+            self.value += self.redoList[-1][1]
+        else:
+            self.value -= self.redoList[-1][1]
+        self.undoList += self.redoList[-1:]
+        del self.redoList[-1]
 
     def bulk_undo(self, steps: int):
-        pass
+        for i in range(steps):
+            EventSourcer.undo(self)
 
     def bulk_redo(self, steps: int):
-        pass
+        for i in range(steps):
+            EventSourcer.redo(self)
