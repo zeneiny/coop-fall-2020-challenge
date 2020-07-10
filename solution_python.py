8732b064c9d28065d737fa021a24cb9fcd26e74d
Mohamed El-Zeneiny <melzeneiny@uwaterloo.ca>
2020-07-10 14:11:22 -0400

    further improved efficiency (S+T)

--- solution_python.py
+++ solution_python.py
@@ -11,37 +11,31 @@ class EventSourcer():
         self.value += num
         self.position +=1
         if (self.position == len(self.actionList)):
-            self.actionList += [("+",num)]
+            self.actionList += [num]
         else:
-            self.actionList[self.position+1] = ("+",num)
+            self.actionList[self.position+1] = num
 
 
     def subtract(self, num: int):
         self.value -= num
         self.position +=1
         if (self.position == len(self.actionList)):
-            self.actionList += [("-",num)]
+            self.actionList += [-num]
         else:
-            self.actionList[self.position+1] = ("-",num)
+            self.actionList[self.position+1] = -num
 
 
     def undo(self):
         if (self.position == -1):
             return
-        if (self.actionList[self.position][0] == "+"):
-            self.value -= self.actionList[self.position][1]
-        else:
-            self.value += self.actionList[self.position][1]
+        self.value -= self.actionList[self.position]
         self.position -= 1
 
 
     def redo(self):
         if (self.position == len(self.actionList) - 1):
             return
-        if (self.actionList[self.position+1][0] == "+"):
-            self.value += self.actionList[self.position][1]
-        else:
-            self.value -= self.actionList[self.position][1]
+        self.value += self.actionList[self.position]
         self.position += 1
 
     def bulk_undo(self, steps: int):
